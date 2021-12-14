e01ea5d324765242bb9b3bfc8af827397f0514f2
Tomasz Ziemek <tomasz.z@protonmail.com>
2021-12-14 13:37:59 +0100

    Update addmeasurementdialog.cpp
    zdefiniowac baze danych z polami Not NULL

--- UI/RecordTDK/addmeasurementdialog.cpp
+++ UI/RecordTDK/addmeasurementdialog.cpp
@@ -21,7 +21,19 @@ void AddMeasurementDialog::on_cancel_pushButton_clicked()
 
 void AddMeasurementDialog::on_start_pushButton_clicked()
 {
+    StarlingLab::DBRecordTDK db;
+    StarlingLab::TDKLogModel log;
+    log.creationDateTime = QDate::currentDate().toString("yyyy-MM-dd");
+    log.nuclide = ui->nuclide_lineEdit->text();
+    log.solution = ui->solution_lineEdit->text();
+    log.noSources = ui->noSources_spinBox->value();
+    log.sourceId = ui->idSources_lineEdit->text();
+    log.sourcePreparationDate = ui->sourcePreparation_dateEdit->date().toString("yyyy-MM-dd");
+    log.description = ui->comments_textEdit->placeholderText();
+    log.linked = ui->linked_lineEdit->text();
+    log.category = ui->category_comboBox->currentIndex();
 
+    // protocolId
 }
 
 
