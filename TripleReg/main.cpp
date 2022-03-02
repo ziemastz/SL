97f2663a7ca9da939dbce5564df383d47fe2c294
Tomasz Ziemek <tomasz.z@protonmail.com>
2022-03-02 14:30:13 +0100

    Update main.cpp

--- TripleReg/main.cpp
+++ TripleReg/main.cpp
@@ -7,8 +7,44 @@
 #include "dialogsinginuser.h"
 #include "databasestarlinglab.h"
 
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+      QByteArray localMsg = msg.toLocal8Bit();
+      QString txt;
+      QDateTime date;
+
+      QFile outFile(QDir::currentPath() + "/log.txt");
+      if(outFile.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Append))
+      {
+          QTextStream out(&outFile);
+          switch (type) {
+          case QtDebugMsg:
+              txt.append(QString("Debug: %1").arg(localMsg.constData()));
+              break;
+          case QtInfoMsg:
+              txt.append(QString("Info: %1").arg(localMsg.constData()));
+              break;
+          case QtWarningMsg:
+              txt.append(QString("Warning: %1").arg(localMsg.constData()));
+              break;
+          case QtCriticalMsg:
+              txt.append(QString("Critical: %1").arg(localMsg.constData()));
+              break;
+          case QtFatalMsg:
+              txt.append(QString("Fatal: %1").arg(localMsg.constData()));
+              abort();
+          }
+          txt.prepend(" - ");
+          txt.prepend(date.currentDateTime().toString("dd-MM-yyyy hh:mm:ss.zzz"));
+          txt.append("\n");
+          out << txt;
+          outFile.close();
+      }
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(myMessageOutput);
     QCoreApplication::addLibraryPath("/bin");
     QApplication a(argc, argv);
     QApplication::setApplicationName("TripleReg");
